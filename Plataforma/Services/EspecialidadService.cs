c7a2a47bc36c8ecc32ab0137f66a355432116522
DevJoseRivera <dev.jose.rivera@gmail.com>
2022-09-29 11:12:32 -0600

    AFP-Crecer: adición de capa de servicios y adición de configuración para entidad Especialidad.

---
+++ Plataforma/Services/EspecialidadService.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Core;
+using Domain.Core.MedicalCenter.Models;
+using Microsoft.EntityFrameworkCore;
+using Services.MedicalCenter.Contracts;
+
+namespace Services
+{
+    public class EspecialidadService : IGenericService<Especialidad>
+    {
+        private readonly AfpCrecerDbContext _ctx;
+
+        public EspecialidadService(AfpCrecerDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<IEnumerable<Especialidad>> GetItemsAsync()
+            => await _ctx.Especialidad.ToListAsync();
+
+        public async Task<Especialidad?> GetItemAsync(int id)
+            => await _ctx.Especialidad.FirstOrDefaultAsync(item => item.Id == id);
+
+        public async Task<Especialidad> SaveItemAsync(Especialidad entity)
+        {
+            var item = _ctx.Especialidad.Add(entity);
+
+            await _ctx.SaveChangesAsync();
+
+            return item.Entity;
+        }
+
+        public async Task UpdateItemAsync(Especialidad entity)
+        {
+            _ctx.Especialidad.Update(entity);
+
+            await _ctx.SaveChangesAsync();
+        }
+
+        public async Task DeleteItemAsync(int id)
+        {
+            var item = await GetItemAsync(id);
+
+            if (item is null)
+                throw new InvalidOperationException("Not Found");
+
+            _ctx.Especialidad.Remove(item);
+
+            await _ctx.SaveChangesAsync();
+        }
+    }
+}
---
+++ Plataforma/Services/MedicalCenter/Contracts/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿namespace Services.MedicalCenter.Contracts
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetItemsAsync();
+        Task<T?> GetItemAsync(int id);
+        Task<T> SaveItemAsync(T entity);
+        Task UpdateItemAsync(T entity);
+        Task DeleteItemAsync(int id);
+    }
+}
